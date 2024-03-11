f07908cd12096efc0a80f05ac0a1e2d32947c010
unknown <shikdermdoliullah@gmail.com>
2024-03-11 12:20:34 +0600

    add bg color in the visited country

--- src/Comphonents/Countries/Countries.jsx
+++ src/Comphonents/Countries/Countries.jsx
@@ -16,16 +16,20 @@ const Countries = () => {
     }
 console.log(showCountry)
     useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all')
+         fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => setShowCountry(data))
-    },[])
+    },[]);
     return (
     <div>
         <p className="text-center font-bold text-red-600 text-3xl">visited country : {countryS.length}</p>
-        <p className="text-center font-bold text-blue-600">{
-            countryS.map(s => <li key={s.ccn3}>Name of visited country : {s.name.common}</li>)
-            }</p>
+        
+       <div>
+       <div className=" block justify-center text-center font-bold text-blue-600">{
+            countryS.map(s => <li className="flex gap-6 my-5 w-2/3 items-center" key={s.ccn3}>Name of visited country : {s.name.common}<span><img className="h-11 w-14" src={s?.flags?.png} alt="" /></span> </li>)
+            }</div>
+            
+       </div>
             <div className="grid md:grid-cols-4 gap-3">
             {
                 showCountry.map(country => <Country  
